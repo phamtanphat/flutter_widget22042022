85015d06953ce0f9aeb43db74391bfd62ba94db0
phamtanphat <phatdroid94@gmail.com>
2022-05-11 21:15:08 +0700

    margin , padding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,35 @@
-import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+//state object
+class _HomePageState extends State<HomePage> {
+
+
+  @override
   Widget build(BuildContext context) {
+    print("build");
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
       ),
       body: Container(
-        child: Text("Home Page Screen"),
+        constraints: BoxConstraints.expand(),
+        color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+              color : Colors.red ,
+              child: Text("ABC")
+          ),
+        )
       ),
     );
   }
